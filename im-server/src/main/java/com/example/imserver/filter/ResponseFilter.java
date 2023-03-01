a597fb48da626bcbd05090d6b8c1e1aa543595bd
yimin.liu <liuyimin@taou.com>
2023-03-02 00:35:56 +0800

    返回值异常不做处理，待优化

--- im-server/src/main/java/com/example/imserver/filter/ResponseFilter.java
+++ im-server/src/main/java/com/example/imserver/filter/ResponseFilter.java
@@ -10,6 +10,8 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
 
+import java.util.Objects;
+
 @ControllerAdvice
 public class ResponseFilter implements ResponseBodyAdvice<Object> {
 
@@ -22,7 +24,7 @@ public class ResponseFilter implements ResponseBodyAdvice<Object> {
     // todo 2023-02-26 17:26:47 异常处理
     @Override
     public Object beforeBodyWrite(Object body, MethodParameter returnType, MediaType selectedContentType, Class selectedConverterType, ServerHttpRequest request, ServerHttpResponse response) {
-        if (returnType.hasMethodAnnotation(ExceptionHandler.class)) {
+        if (Objects.equals(returnType.getMethod().getName(),"error")) {
             //处理异常，可以再添加一个异常处理的类，用于处理异常返回格式
 
             return body;
