8ce8ddda6228493f6cf8ebec8ada3a6cfa43cd0b
yimin.liu <liuyimin@taou.com>
2023-03-02 00:54:52 +0800

    返回值异常不做处理，待优化

--- im-server/src/main/java/com/example/imserver/filter/ResponseFilter.java
+++ im-server/src/main/java/com/example/imserver/filter/ResponseFilter.java
@@ -5,7 +5,6 @@ import org.springframework.core.MethodParameter;
 import org.springframework.http.MediaType;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
-import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseBodyAdvice;
