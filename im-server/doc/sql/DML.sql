796ab76804481b2dece3c55e9634b221be928bf0
HLMin <2432393833@qq.com>
2023-03-06 22:36:01 +0800

    补充user表字段语句

--- im-server/doc/sql/DML.sql
+++ im-server/doc/sql/DML.sql
@@ -5,6 +5,7 @@ create table imy.user
         primary key,
     nickname    varchar(128)                        not null comment '用户名称',
     mobile      char(12)                            null comment '手机号',
+    password    varchar(256)                        null comment '用户密码',
     email       varchar(128)                        null comment '邮箱',
     gender      tinyint                             null comment '性别',
     birthday    timestamp                           null comment '生日',
@@ -32,5 +33,7 @@ create table imy.friend
     unread        int       default 0                 null comment '0 未读；1 已读'
 )
     comment '好友表';
+
+# alter table imy.user add password varchar(256) null comment '用户密码';
 
 
