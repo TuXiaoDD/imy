95af398c83480ffc48377016235a93bf5931b03c
yimin.liu <1357762370@qq.com>
2024-01-04 23:00:52 +0800

    NettyClient

--- im-tcp/src/main/java/com/imtcp/handler/NettyHttpServerHandler.java
+++ im-tcp/src/main/java/com/imtcp/handler/NettyHttpServerHandler.java
@@ -1,14 +1,19 @@
 package com.imtcp.handler;
 
+import io.netty.buffer.ByteBuf;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
 import io.netty.util.concurrent.EventExecutorGroup;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 public class NettyHttpServerHandler extends ChannelInboundHandlerAdapter {
 
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-        super.channelRead(ctx,msg);
+        ByteBuf byteBuf = (ByteBuf)msg;
+        log.info("msg {}",byteBuf.toString());
+        ctx.write(byteBuf);
     }
 
     public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
